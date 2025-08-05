0e50514536426516d6d07badd8c2208b3a0f5dc1
Ahmad Alsabah <ahmad.alsaba03@gmail.com>
2025-08-05 10:27:55 +0300

    Add Rollup config and build script

--- rollup.config.js
+++ rollup.config.js
@@ -1,1 +1,20 @@
 
+import resolve from '@rollup/plugin-node-resolve'
+import { terser } from 'rollup-plugin-terser'
+
+export default {
+  input: 'src/directive.js',
+  output: {
+    file: 'dist/vue-form-ghost.min.js',
+    format: 'umd',
+    name: 'VueFormGhost',
+    globals: {
+      vue: 'Vue'
+    }
+  },
+  external: ['vue'],
+  plugins: [
+    resolve(),
+    terser()
+  ]
+}
